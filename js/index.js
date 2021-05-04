8fc0fb1efd1ac6a48b6a2f53ae6bff7453493bd5
caralocke <cara.m.locke@gmail.com>
2021-05-04 19:16:53 -0400

    was able to get a few other features working

--- js/index.js
+++ js/index.js
@@ -1,39 +1,40 @@
 // Your code goes here
-
+//creating a function to make multiple areas appear the same after different things are done to them
+function makePretty(event) {
+    whiteLetters()
+    pinkBackground()
+}
 // My Selectors
 //header 
 const mainNav = document.querySelector('.main-navigation')
 //nav buttons
-const navLinks = document.querySelector('a')
-
+const navLinks = document.querySelector('nav')
+//top section
+const topSection = document.querySelector('.intro')
 
 // footer
 const footer = document.querySelector('.footer')
 
 // images
 
-const image = document.querySelector('.img')
+const image = document.querySelector('img')
 
 // text content
 const textContent = document.querySelector('.text')
 const contentSection = document.querySelector('.section')
 
-// My Color Changing With 'mouseenter'
+// 1) My Color Changing With 'mouseenter'
 mainNav.addEventListener('mouseenter', event => {
     event.target.style.backgroundColor = 'pink'
     event.target.style.color = 'white'
 })
 navLinks.addEventListener('mouseenter', event => {
     event.target.style.backgroundColor = 'pink'
-    event.target.style.color = 'white'
+    navLinks.textContent.style.color = 'white'
 })
-// contentSection.addEventListener('mouseenter', event => {
-//     event.target.style.backgroundColor = 'pink'
-//     event.target.style.color = 'white'
-// })
-//My Color Changing With 'mouseleave'
+// 2) My Color Changing With 'mouseleave'
 mainNav.addEventListener('mouseleave', event => {
-    event.target.style.backgroundColor = 'white'
+    event.target.style.backgroundColor = 'initial'
     event.target.style.color = 'black'
 })
 navLinks.addEventListener('mouseenter', event => {
@@ -41,31 +42,55 @@ navLinks.addEventListener('mouseenter', event => {
     event.target.style.color = 'black'
 })
 
-// keydown
 
-// wheel
-
-
-//resize
-
-//select
-
-//mouseover
+// 3) mouseover
 
 footer.addEventListener('mouseover', event => {
     event.target.style.backgroundColor = 'pink'
     event.target.style.color = 'white'
 })
 
-//mouseout
+// 4) mouseout
 footer.addEventListener('mouseout', event => {
-    event.target.style.backgroundColor = 'white'
+    event.target.style.backgroundColor = 'initial'
     event.target.style.color = 'black'
 })
 
-//Stop the navigation items from refreshing the page by using `preventDefault()`
+// 5) click
+// Also stop the navigation items from refreshing the page by using `preventDefault()`
 Array.from(document.links).forEach(function(link){
     link.addEventListener('click', function (event) {
         event.preventDefault()
     })
 })
+
+//  6) focus
+topSection.addEventListener('focus', (event) => {
+    event.target.style.background = 'pink'
+})
+
+// keydown
+
+// 8) wheel
+window.addEventListener('wheel', function() {
+    document.body.style.color = 'pink'
+})
+
+//resize
+
+//select
+
+
+//Helper functions to make some of the code more readable
+
+function whiteLetters(){
+    target.style.color ='white'
+}
+
+function pinkBackground() {
+    target.style.color ='pink'
+}
+
+function pinkLetters(){
+    target.style.color = 'pink'
+}
