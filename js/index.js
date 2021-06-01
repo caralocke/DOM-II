19cd820b77fec0e1abc3e20696294e474c66b326
caralocke <cara.m.locke@gmail.com>
2021-06-01 18:25:37 -0400

    Added event listeners to my subtitles that change colors when clicked, double clicked, and when the mouse wheel is used on them

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@
 // Selectors
 
 const mainNav = document.querySelector('.main-navigation')
-const navLinks = document.querySelectorAll('a')
 const intro = document.querySelector('.intro')
+const subTitles = document.querySelectorAll('h2')
+const explanation = document.querySelectorAll('p')
+const title = document.querySelector('h1')
+
+title.addEventListener('')
 
 mainNav.addEventListener('mouseenter', event => {
     event.target.style.backgroundColor = 'pink'
@@ -14,9 +18,43 @@ mainNav.addEventListener('mouseleave', event => {
     event.target.style.backgroundColor = 'initial'
     event.target.style.color = 'initial'
 })
-
-navLinks.addEventListener('mouseenter', event => {
-    event.target.style.backgroundColor = 'black'
-    navLinks.textContent.style = 'teal'
+//adding event listeners to my nav links
+Array.from(document.links).forEach(link => {
+    link.addEventListener('mouseenter', event => {
+        event.target.style.color = 'white'
+        event.target.style.backgroundColor = 'cyan'
+    })
+})
+Array.from(document.links).forEach(link => {
+    link.addEventListener('mouseleave', event => {
+        event.target.style.color = 'initial'
+        event.target.style.backgroundColor = 'initial'
+    })
 })
 
+//wheel on my h2's
+console.log('subTitles:', subTitles)
+Array.from(subTitles).forEach(subTitle => {
+    subTitle.addEventListener('wheel', event => {
+        subTitle.style.color = 'teal'
+        event.stopPropagation()
+    })
+})
+
+//click to change the area to cyan background with white text
+
+intro.addEventListener('click', event => {
+    event.target.style.backgroundColor = 'cyan'
+    event.target.style.color = 'white'
+})
+
+//dblclick to change the listed areas back to their initial settings
+
+intro.addEventListener('dblclick', event => {
+    event.target.style.backgroundColor = 'initial'
+    event.target.style.color = 'initial'
+})
+console.log(subTitles)
+document.addEventListener('scroll', event => {
+    subTitles.textContent.style.color = 'pink'
+})
