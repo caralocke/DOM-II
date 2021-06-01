d39297ece200a3155df2834927fb61725a819735
caralocke <cara.m.locke@gmail.com>
2021-06-01 17:09:29 -0400

    npm installed and started making my selectors as well as adding a few event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+// Selectors
+
+const mainNav = document.querySelector('.main-navigation')
+const navLinks = document.querySelectorAll('a')
+const intro = document.querySelector('.intro')
+
+mainNav.addEventListener('mouseenter', event => {
+    event.target.style.backgroundColor = 'pink'
+    event.target.style.color = 'white'
+})
+
+mainNav.addEventListener('mouseleave', event => {
+    event.target.style.backgroundColor = 'initial'
+    event.target.style.color = 'initial'
+})
+
+navLinks.addEventListener('mouseenter', event => {
+    event.target.style.backgroundColor = 'black'
+    navLinks.textContent.style = 'teal'
+})
+
